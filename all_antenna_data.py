b530de5681e92ca3116e100dd299abab16238904
Kasey Wei <kwei@nrao.edu>
2023-06-12 14:10:20 -0400

    fixes to horizontal flipping and bottom map portion display

--- all_antenna_data.py
+++ all_antenna_data.py
@@ -43,9 +43,8 @@ print(f"Data frame memory usage: {format_bytes(df.memory_usage(index=True).sum()
 
 # %%
 # remove "invalid" data
-df = df[(df["RAJ2000"] <= 360) & (df["RAJ2000"] >= 0)]
-df = df[(df["DECJ2000"] <= 90) & (df["DECJ2000"] >= -90)]
-df["RAJ2000"] = df["RAJ2000"] - 180
+df = df[(df["RAJ2000"] >= 0) & (df["RAJ2000"] <= 360)]
+df = df[(df["DECJ2000"] >= -90) & (df["DECJ2000"] <= 90)]
 
 # %%
 start = time.perf_counter()
@@ -55,16 +54,19 @@ print(f"Rendering df in geoviews: {round(time.perf_counter() - start, 2)}s")
 points = points.opts(gv.opts.Points(projection=crs.Mollweide(), global_extent=True, width=2000, height=1000))
 
 start = time.perf_counter()
-projected = gv.operation.project_points(points, projection=crs.Mollweide())
+projected = gv.operation.project_points(points, projection=crs.Mollweide(), global_extent=True)
 print(f"Projecting geoviews points: {round(time.perf_counter() - start,2)}s")
 
-# %%
-canvas = ds.Canvas(plot_width=2000, plot_height=1000)
-canvas_points = canvas.points(projected.data, "RAJ2000", "DECJ2000")
+ranges_list = [[0, -90], [180, 90]]
+ranges_df = pd.DataFrame(ranges_list, columns=["RAJ2000", "DECJ2000"])
+ranges_points = gv.Points(ranges_df, kdims=['RAJ2000', 'DECJ2000'])
+ranges_points = ranges_points.opts(gv.opts.Points(projection=crs.Mollweide()))
+ranges_projected = gv.operation.project_points(ranges_points, projection=crs.Mollweide())
+ranges = ranges_projected.data
 
 # %%
-shaded = ds.transfer_functions.shade(canvas_points, cmap=cc.bgy)
-shaded
+canvas = ds.Canvas(plot_width=2000, plot_height=1000, y_range=(min(ranges["DECJ2000"]), max(ranges["DECJ2000"])))
+canvas_points = canvas.points(projected.data, "RAJ2000", "DECJ2000")
 
 # %%
 ds.tf.Images(ds.tf.set_background(ds.tf.shade(canvas_points, cc.bmw), "black"),
