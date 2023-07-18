8526885f2fc27575bde616d2e52b35af2cbb2d0e
Kasey Wei <kasey.wei121@gmail.com>
2023-07-18 10:39:31 -0400

    widgets for scan number and datetime

--- ant_pos_panel_server.py
+++ ant_pos_panel_server.py
@@ -38,8 +38,8 @@ dataset = remove_invalid_data(dataset)
 
 cmaps = ["rainbow4", "bgy", "bgyw", "bmy", "gray", "kbc"]
 sessions = [""] + dataset["Session"].unique().tolist()
-# frontends = [""] + dataset["Frontend"].unique().tolist()
-# backends = [""] + dataset["Backend"].unique().tolist()
+# frontends = dataset["Frontend"].unique().tolist()
+# backends = dataset["Backend"].unique().tolist()
 # proc_names = [""] + dataset["ProcName"].unique().tolist()
 
 # FAKE data
@@ -51,11 +51,12 @@ proc_names = ["", "i'm", "a", "little", "teacup"]
 dataset["Observer"] = np.random.choice(observers, size=df_size)
 dataset["Frontend"] = np.random.choice(frontends, size=df_size)
 dataset["Backend"] = np.random.choice(backends, size=df_size)
-# dataset["ProcName"] = np.random.choice(proc_names, size=df_size)
+dataset["ProcName"] = np.random.choice(proc_names, size=df_size)
 dataset["ScanNumber"] = np.random.randint(low=0, high=1000, size=df_size)
 dataset["ScanStart"] = [dt.datetime(2007, 4, 24)] * int(df_size / 2) + [
     dt.datetime(2023, 7, 1)
 ] * (df_size - int((len(dataset) / 2)))
+
 
 class AntennaPositionExplorer(param.Parameterized):
     cmap = param.Selector(default=cm["rainbow4"], objects={c: cm[c] for c in cmaps})
@@ -63,9 +64,12 @@ class AntennaPositionExplorer(param.Parameterized):
     observer = param.String("")
     frontend = param.ListSelector(default=frontends, objects=frontends)
     backend = param.ListSelector(default=backends, objects=backends)
-    # scan_number = param.Range(default=(0,1000), bounds=(0,1000))
-    # scan_start = param.Range()
-    # proc_name = param.String("")
+    scan_number = param.Range(default=(0, 1000), bounds=(0, 1000))
+    scan_start = param.DateRange(
+        default=(dt.datetime(2002, 1, 1), dt.datetime(2023, 7, 17)),
+        bounds=(dt.datetime(2002, 1, 1), dt.datetime.today()),
+    )
+    proc_name = param.String("")
 
     # RA/DEC?
 
@@ -75,10 +79,16 @@ class AntennaPositionExplorer(param.Parameterized):
             & (dataset["Observer"].str.contains(self.observer))
             & (dataset["Frontend"].isin(self.frontend))
             & (dataset["Backend"].isin(self.backend))
-            # & (dataset["ScanNumber"].isin(range(self.scan_number[0], self.scan_number[1])))
-            # & (dataset["ScanStart"].isin(range(self.scan_start)))
-            # & (dataset["ProcName"].str.contains(self.proc_name))
+            & (
+                dataset["ScanNumber"].isin(
+                    range(self.scan_number[0], self.scan_number[1])
+                )
+            )
+            & (dataset["ScanStart"] >= self.scan_start[0])
+            & (dataset["ScanStart"] <= self.scan_start[1])
+            & (dataset["ProcName"].str.contains(self.proc_name))
         ].copy()
+        print(df)
         return df
 
     def view(self, **kwargs):
@@ -104,21 +114,19 @@ widgets = pn.Param(
                 options=observers, restrict=False, name="Observer"
             )
         },
-        # "scan_number": {
-        #     "type": pn.widgets.IntRangeSlider(start=0, end=1000, name="Number of scans")
-        # },
-        # "scan_start": {
-        #     "type": pn.widgets.DatetimeRangeInput(
-        #         start=dt.datetime(2002, 1, 1),
-        #         end=dt.datetime.today(),
-        #         name="Number of scans",
-        #     )
-        # },
-        # "proc_name": {
-        #     "type": pn.widgets.AutocompleteInput(
-        #         options=proc_names, restrict=False, name="Proc names?"
-        #     )
-        # },
+        "scan_start": {
+            "type": pn.widgets.DatetimeRangeInput(
+                start=dt.datetime(2002, 1, 1),
+                end=dt.datetime.today(),
+                value=(dt.datetime(2002, 1, 1), dt.datetime(2023, 7, 17)),
+                name="Date and time of scan",
+            )
+        },
+        "proc_name": {
+            "type": pn.widgets.AutocompleteInput(
+                options=proc_names, restrict=False, min_characters=0, name="Proc names?"
+            )
+        },
     },
 )
 pn.Row(widgets, ant_pos.view).servable()
